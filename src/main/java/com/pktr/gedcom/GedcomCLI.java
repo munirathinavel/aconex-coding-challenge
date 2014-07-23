757d6061c87babdc017672e015a4790445233d13
Pradeep Kumar <pkumar@homeaway.com>
2014-07-23 12:50:33 +0530

    Ability to provide file path from command-line

--- src/main/java/com/pktr/gedcom/GedcomCLI.java
+++ src/main/java/com/pktr/gedcom/GedcomCLI.java
@@ -15,8 +15,16 @@ public class GedcomCLI {
     public static final String GEDCOM_OUTPUT_FILE = "GEDCOM Parser Challenge sample data.xml";
 
     public static void main(String[] args) throws ParserConfigurationException {
-        XMLCreator xmlCreator = new XMLCreator(GEDCOM_INPUT_FILE, GEDCOM_OUTPUT_FILE, new NodeParser());
-        System.out.println(String.format("Input TXT File %s parsed and the output XML is created %s",
-            GEDCOM_INPUT_FILE, xmlCreator.generateXMLDocument()));
+        XMLCreator xmlCreator = null;
+        NodeParser nodeParser = new NodeParser();
+        if (args.length > 2 || args.length < 2) {
+            xmlCreator = new XMLCreator(GEDCOM_INPUT_FILE, GEDCOM_OUTPUT_FILE, nodeParser);
+            System.out.println(String.format("Input TXT File %s parsed and the output XML is created %s",
+                GEDCOM_INPUT_FILE, xmlCreator.generateXMLDocument()));
+        } else {
+            xmlCreator = new XMLCreator(args[0], args[1], nodeParser);
+            System.out.println(String.format("Input TXT File %s parsed and the output XML is created %s",
+                args[0], xmlCreator.generateXMLDocument()));
+        }
     }
 }
